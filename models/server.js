aa65870f484683b078047a6a887e0ccd1c0ff244
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-16 14:41:43 -0500

    make it better

--- models/server.js
+++ models/server.js
@@ -6,8 +6,8 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.pathUsers = '/users';
-        this.authPath = '/auth';
+        this.pathUsers = 'api/users';
+        this.authPath = 'api/auth';
 
         //Database connection
         this.databaseConnection();
